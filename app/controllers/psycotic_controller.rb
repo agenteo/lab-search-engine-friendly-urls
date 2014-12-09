a621cb91ccaf0db50fbed1dc8e1c78e8bff291b1
Enrico Teotti <eteotti@xogrp.com>
2014-12-09 09:21:15 -0500

    Stub on how redirect could work.

--- app/controllers/psycotic_controller.rb
+++ app/controllers/psycotic_controller.rb
@@ -1,6 +1,11 @@
 class PsycoticController < ApplicationController
   def index
     lookup = PsycoticLookup.new(params[:psycotic_slug])
+    # redirect = PsycoticRedirect.new(params[:psycotic_slug], lookup.ids)
+    # if redirect.execute?
+    #   redirect_to "/digs/#{redirect.destination}"
+    # end
+    # do your search with lookup.ids
     render text: lookup.ids
   end
 end
